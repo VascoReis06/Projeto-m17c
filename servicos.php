6188ddbeecb5bf5f5d7ecba9c89faaf9a1d6408c
MastaryRato <156662086+MastaryRato@users.noreply.github.com>
2024-03-18 12:24:37 +0000

    Criação da Carcaça do Site
    Inseri alguns ficheiros + pastas

---
+++ servicos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+</header>
+<nav>
+<a href="inicio.php">Início</a>
+    <a href="sobre.php">Sobre</a>
+    <div class="dropdown">
+        <a href="servicos">Serviços</a>
+        <div class="dropdown-content">
+            <a href="servico1.php">Serviço 1</a>
+            <a href="servico2.php">Serviço 2</a>
+            <a href="servico3.php">Serviço 3</a>
+        </div>
+    </div>
+    <a href="avaliacoes.php">Avaliações</a>
+    <a href="contatos.php">Contatos</a>
+</nav>
+</body>
+</html>
