c9837006321a365aaf2afa8ea763669de0f75e36
MastaryRato <156662086+MastaryRato@users.noreply.github.com>
2024-03-19 10:11:56 +0000

    Adição de alguns elementos informativos a respeito do site
    Nota: Preencher o interior da "Carcaça"

--- inicio.php
+++ inicio.php
@@ -6,25 +6,40 @@
     <title>Página Inicial</title>
 </head>
 <header>
-</header>
-<nav>
-    <a href="inicio.php">Início</a>
-    <a href="sobre.php">Sobre</a>
-    <div class="dropdown">
-        <a href="servicos">Serviços</a>
-        <div class="dropdown-content">
-            <a href="servico1.php">Serviço 1</a>
-            <a href="servico2.php">Serviço 2</a>
-            <a href="servico3.php">Serviço 3</a>
+    <nav>
+        <a href="inicio.php">Início</a>
+        <a href="sobre.php">Sobre</a>
+        <div class="dropdown">
+            <a href="servicos">Serviços</a>
+            <div class="dropdown-content">
+                <a href="servico1.php">Serviço 1</a>
+                <a href="servico2.php">Serviço 2</a>
+                <a href="servico3.php">Serviço 3</a>
+            </div>
         </div>
-    </div>
-    <a href="avaliacoes.php">Avaliações</a>
-    <a href="contatos.php">Contatos</a>
-</nav>
+        <a href="avaliacoes.php">Avaliações</a>
+        <a href="contatos.php">Contatos</a>
+    </nav>
+</header>
+
 <body>
-    <h2>Bem-vindo à nossa empresa de limpezas</h2>
-    <h3>REALIZAMOS TODOS SERVIÇOS DE LIMPEZA!</h3>
-    <h4>A SuperLimpa realiza todos serviços de limpeza a 7€. É uma empresa de limpeza com mais de 20 anos de experiência, que realiza todo e qualquer tipo serviço de limpeza! Prestamos serviços de limpeza em Lisboa, Porto, Algarve e nas ilhas dos Açores e da Madeira, sendo que, atualmente, já cobrimos todo o território continental.
-    Somos uma empresa de limpeza de Lisboa com uma vasta experiência em qualquer área de intervenção, tal como, nas limpezas empresariais, domésticas e industriais, temos profissionais com alta competência e experiência técnica, estamos munidos com as máquinas e produtos de última geração! Leia mais<h4>
+    <br><br>
+    <h7>Melhor Serviço a Preço Justo</h7>
+    <h2>Empresa de Limpeza de Vidros</h2>
+    <h3>Somos Especialistas em Serviços de Limpezas de Vidros em Portugal.
+    <p> Temos Mais de 70 Anos de Experiência Em Limpezas.
+    <p> +700 Empresas Confiam na TVR-Limpezas Para Manter as Suas Instalações Limpas & Seguras</h3>
+    <button>Contacte-nos</button>
+    <br><br><hr>
+    <h1>70 Anos Experiência | 70 000 Clientes Satisfeitos | 33% Poupanças nos Custos</h1>
+    <hr>
+    <h2>O que nos distingue como Empresa de Limpezas? | Equipas Qualificadas e Comprometidas</h2>
+    <h3>Investimos na formação regular e contínua das nossas equipas, as quais não só são qualificadas e experientes, mas também atentas às necessidades específicas dos nossos clientes, sendo reconhecidas pela sua qualidade.</h3>
+    <h2>Flexibilidade Horária</h2>
+    <h3>Estamos disponíveis 24 horas por dia, 7 dias por semana, para garantir que os seus espaços se encontram sempre impecáveis. Adaptamo-nos à rotina de cada empresa garantindo assim a máxima conveniência e satisfação dos nossos clientes.</h3>
 </body>
+
+<footer>
+        <p>Este é o rodapé do site.</p>
+    </footer>
 </html>
